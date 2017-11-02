7b6103d411b3924db86b7d86cc6cbba55734ff81
flps2xd <33330838+flps2xd@users.noreply.github.com>
2017-11-02 18:33:57 -0200

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const prefix = "gg" 
-client.on("ready", () => {
-  console.log(`Em funcionamento!!!! total de ${client.channels.size} Canais, em ${client.guilds.size} servers, um total de ${client.users.size} usuarios.`)
-})
- 
- 
-client.login("Mzc1NzEwMjk5Mjk2MjM1NTMz.DN0JJg.R6XRRPMmpgzOofVy9AFN9fBGc5c")
- 
- 
-client.on("message", (message) => {
-    if (message.author.bot) return;
-    if (message.channel.type !== 'text') return;
-    if (!message.content.startsWith("gg")) return
- 
-  if (message.content.startsWith("gg" + "kobra")) {
-    message.channel.sendMessage("lixo")
-  }
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
