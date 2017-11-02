6ecc31934a08e49083673f2429bf123e16724a98
flps2xd <33330838+flps2xd@users.noreply.github.com>
2017-11-02 18:22:11 -0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const prefix = "gg" 
+client.on("ready", () => {
+  console.log(`Em funcionamento!!!! total de ${client.channels.size} Canais, em ${client.guilds.size} servers, um total de ${client.users.size} usuarios.`)
+})
+ 
+ 
+client.login("Mzc1NzEwMjk5Mjk2MjM1NTMz.DN0JJg.R6XRRPMmpgzOofVy9AFN9fBGc5c")
+ 
+ 
+client.on("message", (message) => {
+    if (message.author.bot) return;
+    if (message.channel.type !== 'text') return;
+    if (!message.content.startsWith("gg")) return
+ 
+  if (message.content.startsWith("gg" + "kobra")) {
+    message.channel.sendMessage("lixo")
+  }
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
